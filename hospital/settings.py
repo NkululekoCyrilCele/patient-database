d5726dc5ce11d50cfb034a3ffa41fadac2659ad7
Nkululeko Cyril Cele <nkululeko.cele@gmail.com>
2023-08-01 22:36:47 +0200

    Update settings

--- hospital/settings.py
+++ hospital/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@@hd#!0#r%2qz&du^qrtyyy(ppn&_4+itojrc_9*o++hwar#*m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['patient-database.onrender.com']
 
 
 # Application definition
