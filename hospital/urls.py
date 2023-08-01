944378780ede4840686379be04a16d2d4071620c
Nkululeko Cyril Cele <nkululeko.cele@gmail.com>
2023-08-02 01:16:38 +0200

    Update settings and urls

--- hospital/urls.py
+++ hospital/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('patients.urls')),
 ]
 
-urlpatterns += staticfiles_urlpatterns()
+# urlpatterns += staticfiles_urlpatterns()
